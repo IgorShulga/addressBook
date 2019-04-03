98126b7a8464c2e1a63217d9b8148b98cc540e2e
ihor <igor.shulga8@gmail.com>
2019-04-03 10:22:43 +0300

    add methods (findById, showAllContacts, deleteById, updataContact, deleteContact) in class ContactDao

--- src/main/java/dao/ContactDao.java
+++ src/main/java/dao/ContactDao.java
@@ -20,6 +20,52 @@ public class ContactDao {
         }
     }
 
+    public void findById(int id) {
+        for (int index = 0; index < store.length; index++) {
+            if (index == id) {
+                System.out.print("This contact found by your ID: ");
+                System.out.println(store[index].toString());
+                break;
+            }
+        }
+    }
+
+    public void showAllContacts() {
+        for (Contact contact : store) {
+            System.out.println(contact.toString());
+        }
+    }
+
+    public void deleteById(int id) {
+        for (int index = 0; index < store.length; index++) {
+            if (index == id) {
+                store[index] = null;
+                System.out.println("Contact with ID = " + index + " deleted.");
+                break;
+            }
+        }
+    }
+
+    public void updataContact(Contact contact) {
+        for (int argument = 0; argument < store.length; argument++) {
+            if (store[argument].getId() == contact.getId()) {
+                store[argument] = contact;
+                System.out.println("Your contact updated in your address book");
+                System.out.println(contact.toString());
+                break;
+            }
+        }
+    }
+
+    public void deleteContact(Contact contact) {
+        for(int argument = 0; argument < store.length; argument++){
+            if(store[argument].getId() == contact.getId()){
+                store[argument] = null;
+                System.out.println("Your contact deleted");
+            }
+        }
+    }
+
     public Contact[] getStore() {
         return store;
     }
