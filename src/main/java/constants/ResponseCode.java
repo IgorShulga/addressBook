0048126985073d449ef0d240674b8da5be11ce05
IgorShulga <igor.shulga8@gmail.com>
2019-06-09 12:53:08 +0300

    refactoring error processing

--- src/main/java/constants/ResponseCode.java
+++ src/main/java/constants/ResponseCode.java
@@ -1,6 +1,11 @@
 package constants;
 
 public enum ResponseCode {
+    FAILED_SAVE_DB(445),
+    FAILED_DELETE_DB(446),
+    FAILED_GET_DB(447),
+    FAILED_GET_DATA(448),
+    FAILED_UPDATE_DB(449),
     NOT_FOUND(404),
     OBJECT_EXIST(302),
     WRONG_DATA_TYPE(444),
